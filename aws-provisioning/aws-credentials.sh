e87ee5f6d9ca66be5fcb9712505c1c4d5906ed3a
Giacomo Marciani <giacomo.marciani@gmail.com>
2017-04-09 21:29:23 +0200

    aws basic system provisioning

--- aws-provisioning/aws-credentials.sh
+++ aws-provisioning/aws-credentials.sh
@@ -2,4 +2,4 @@
 export AWS_ACCESS_KEY_ID='AKIAJJMP7DHOJD5CX5SQ'
 export AWS_ACCESS_KEY_SECRET='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'
 export AWS_KEYPAIR_NAME='gmarciani'
-export AWS_KEYPAIR_PATH='~/.aws/gmarciani.pem'
+export AWS_KEYPAIR_PATH='/home/gmarciani/.aws/gmarciani.pem'
