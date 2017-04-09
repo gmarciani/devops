ddf3c4c2ae0654132b33536c4b3b4c528c9296d1
Giacomo Marciani <giacomo.marciani@gmail.com>
2017-04-09 21:50:24 +0200

    removed credentials

--- aws-provisioning/credentials.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export AWS_ACCESS_KEY_ID='AKIAJJMP7DHOJD5CX5SQ'
-export AWS_ACCESS_KEY_SECRET='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'
-export AWS_KEYPAIR_NAME='gmarciani'
-export AWS_KEYPAIR_PATH='/home/gmarciani/.aws/gmarciani.pem'
--- do-provisioning/credentials.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export DO_PRIVATE_KEYPATH='~/.ssh/vagrant@digitalocean'
-export DO_TOKEN='302cc0af2e74400fdedc4a9b9feead1eeabc3559507701bc658884cb8055ae7c'
