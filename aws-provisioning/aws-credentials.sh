e622a0223657ff1f87a79b84551322e8710b839d
Giacomo Marciani <giacomo.marciani@gmail.com>
2017-04-09 19:20:41 +0200

    aws ec2 basic provisioning

---
+++ aws-provisioning/aws-credentials.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export AWS_ACCESS_KEY_ID='AKIAJJMP7DHOJD5CX5SQ'
+export AWS_ACCESS_KEY_SECRET='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'
+export AWS_KEYPAIR_NAME='gmarciani'
+export AWS_KEYPAIR_PATH='~/.aws/gmarciani.pem'
