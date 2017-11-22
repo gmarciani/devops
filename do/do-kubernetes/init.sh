5d85a15e6671366435b27e4c025b9cc195ab9ba0
Giacomo Marciani <giacomo.marciani@gmail.com>
2017-11-22 22:04:08 +0100

    stable kubernetes

--- do/do-kubernetes/init.sh
+++ do/do-kubernetes/init.sh
@@ -13,3 +13,8 @@ cd ${DO_HOME}
 ansible-playbook ${PLAYBOOK_DO_INIT}
 
 cd ${CWD}
+
+unset DO_KUBERNETES_HOME
+unset DO_HOME
+unset PLAYBOOK_DO_INIT
+unset CDW
--- do/do-kubernetes/provision.sh
+++ do/do-kubernetes/provision.sh
@@ -8,8 +8,15 @@ PLAYBOOK_PROVISION="${KUBERNETES_HOME}/cluster.yml"
 
 CWD=$(pwd)
 
-cd ${DO_HOME}
+cd ${KUBERNETES_HOME}
 
 ansible-playbook ${PLAYBOOK_PROVISION}
 
 cd ${CWD}
+
+export KUBECONFIG="~/.kube/config:${KUBERNETES_HOME}/artifacts/admin.conf"
+
+unset DO_KUBERNETES_HOME
+unset KUBERNETES_HOME
+unset PLAYBOOK_PROVISION
+unset CDW
